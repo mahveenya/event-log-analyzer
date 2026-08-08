8e49200a111c61440ced3360b5fbb780d05426b7
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2026-08-08 23:37:23 +0300

    feat: add Event dataclass with row parsing

--- event_log/event.py
+++ event_log/event.py
@@ -1,2 +1,35 @@
+from dataclasses import dataclass
+from typing import Self
+
+from event_log import EventSeverity
+
+
+@dataclass
 class Event:
-    pass  # TODO
+    event_number: str
+    timestamp: str
+    severity: EventSeverity
+    object_name: str
+    object_id: str
+    event_id: str
+    event_name: str | None
+    description: str | None
+    remedy: str | None
+    readable: str | None
+    raw: str | None
+
+    @classmethod
+    def from_row(cls, row: dict[str, str]) -> Self:
+        return cls(
+            event_number=row["Event number"],
+            timestamp=row["Timestamp"],
+            severity=EventSeverity(row["Severity"]),
+            object_name=row["Object name"],
+            object_id=row["Object ID"],
+            event_id=row["Event ID"],
+            event_name=row.get("Event name"),
+            description=row.get("Description"),
+            remedy=row.get("Remedy"),
+            readable=row.get("Readable"),
+            raw=row.get("Raw"),
+        )
