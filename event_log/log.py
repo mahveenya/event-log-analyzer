599cdcdef3ba65c4b7a586730317eb312f86482a
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2026-08-09 15:17:50 +0300

    feat: implement filter_by_severity method

--- event_log/log.py
+++ event_log/log.py
@@ -1,9 +1,12 @@
+from event_log import Event, EventSeverity
+
+
 class EventLog:
     def __init__(self, events):
         self._events = events
 
-    def filter_by_severity(self, min_severity):
-        pass  # TODO
+    def filter_by_severity(self, min_severity: EventSeverity) -> list[Event]:
+        return [event for event in self._events if event.severity >= min_severity]
 
     def count_by_object(self):
         pass  # TODO
